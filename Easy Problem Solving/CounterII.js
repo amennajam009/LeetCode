28dc85a408c98dbb8e03816332d4452137478b41
Amen Najam <98958807+trench009@users.noreply.github.com>
2023-06-15 14:34:22 +0500

    add

--- Easy Problem Solving/CounterII.js
+++ Easy Problem Solving/CounterII.js
@@ -1,5 +1,4 @@
 // In this we're calling 3 function in one function we'll increment decrement and reset the value of function
-
 const create3Function = (value) =>{
  let currentvalue = value
    
@@ -26,3 +25,18 @@ const counterII = create3Function(10)
 console.log(counterII.increment())
 console.log(counterII.decrement())
 console.log(counterII.reset())
+
+
+// Optimized Way 
+
+const Myfunction = (value) =>{
+   let numberToPlay = value
+   return{
+    increment:()=> ++numberToPlay,
+    decrement:()=> --numberToPlay,
+    reset:()=> numberToPlay = value
+   }   
+}
+const counter = Myfunction(10)
+console.log(counter.increment())
+console.log(counter.decrement())
