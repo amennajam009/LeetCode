e1ba897ef42eafa12321d0b83561320d13155ab1
Amen Najam <98958807+trench009@users.noreply.github.com>
2023-06-15 15:08:58 +0500

    add

--- Easy Problem Solving/Sort.js
+++ Easy Problem Solving/Sort.js
@@ -5,3 +5,10 @@ const Myfunction = (arr) =>{
 }
 
 console.log(Myfunction([2,3,4,5,3,2,6,7,2,1]))
+
+// Sorting of An Array In Reverse 
+
+const Myfunction2 = (arr) =>{
+ return arr.sort((a,b)=> b-a)
+}
+console.log(Myfunction2([1,2,3,4,5,6,7,8,9]))
